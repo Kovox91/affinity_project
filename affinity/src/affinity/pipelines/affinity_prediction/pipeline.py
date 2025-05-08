a82032e30113fc3ee56a31c1f1809badcc6ff282
Kovox91 <sascha.maschmann@hotmail.de>
2025-05-08 16:34:24 +0200

    Added some structure and boilerplate

---
+++ affinity/src/affinity/pipelines/affinity_prediction/pipeline.py
@@ -0,0 +1,11 @@
+"""
+This is a boilerplate pipeline 'affinity_prediction'
+generated using Kedro 0.19.12
+The Idea is to call the components/submodules into a neat pipeline, most likely in independent singularities
+"""
+
+from kedro.pipeline import node, Pipeline, pipeline  # noqa
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
