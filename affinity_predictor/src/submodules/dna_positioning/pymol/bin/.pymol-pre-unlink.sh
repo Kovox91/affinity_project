fe19e7d28a176bdc80a1e1396e59435a3cb65f32
Kovox91 <sascha.maschmann@hotmail.de>
2025-06-23 10:26:45 +0200

    added .pdb manipulation scripts

--- affinity_predictor/src/submodules/dna_positioning/pymol/bin/.pymol-pre-unlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ "`uname`" == Darwin ]]; then
-    PYMOL_APP="$PREFIX/PyMOL.app"
-    rm -rf "$PYMOL_APP"
-fi
--- affinity_predictor/src/submodules/dna_positioning/pymol/share/pymol/modules/web/pymolhttpd.py
+++
@@ -1,7 +0,0 @@
-print('Please use pymol.pymolhttpd instead of web.pymolhttpd')
-
-from pymol.pymolhttpd import *
-
-if __name__ == 'pymol':
-    server = PymolHttpd()
-    server.start()
